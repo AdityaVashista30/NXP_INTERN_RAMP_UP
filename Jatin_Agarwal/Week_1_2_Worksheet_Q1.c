ac744da6ed8b8a48a7c258f52b9126fbfa5b97b4
jatin21032 <jatin21032@iiitd.ac.in>
2022-12-30 01:06:20 +0530

    week 1 pythonQ1 done

--- Jatin_Agarwal/Week_1_2_Worksheet_Q1.c
+++ Jatin_Agarwal/Week_1_2_Worksheet_Q1.c
@@ -5,12 +5,12 @@ int main(){
 unsigned int addr = 10000; //0b0010011100010000
 unsigned char a = ((addr>>16) & (0xff)); 
  int x = (int)10000>> 16 ;
- printf("x= %d\n",x);
+ printf("x= %d\n",x); //x=0
  int y= x & (0xff) ;
- printf("y= %d\n",y);
+ printf("y= %d\n",y); //y=0
 unsigned char b = (((addr>>8) & (0xff)));
 int z = 10000>> 8 ;
-printf("z= %d\n",z);
+printf("z= %d\n",z); //z=39
 int w = z & (0xff) ;
-printf("w= %d\n",w);
+printf("w= %d\n",w);//w=39
 }
